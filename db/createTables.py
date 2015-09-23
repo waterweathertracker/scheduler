35f21586eb56d5da56a7d6774fe4f054b084c30b
Niall O'Kane <okanenm@gmail.com>
2015-09-22 18:29:19 -0700

    Create 'water_bodies' and 'water_measurements' tables

--- db/createTables.py
+++ db/createTables.py
@@ -8,16 +8,29 @@ engine = create_engine(
 metadata = MetaData()
 
 states_table = Table('states', metadata,
-                    Column('id', Integer, primary_key=True),
-                    Column('name', String(40)),
-                    Column('abbr', String(5))
-                    )
+    Column('id', Integer, primary_key=True),
+    Column('name', String(40)),
+    Column('abbr', String(5))
+)
  
 counties_table = Table('counties', metadata,
-                        Column('id', Integer, primary_key=True),
-                        Column('name', String(100)),
-                        Column('state_id', None, ForeignKey('states.id'))
-                        )
+    Column('id', Integer, primary_key=True),
+    Column('name', String(100)),
+    Column('state_id', None, ForeignKey('states.id'))
+)
  
+bodies_tables = Table('water_bodies', metadata,
+    Column('id', Integer, primary_key=True),
+    Column('name', String),
+    Column('state_id', Integer, ForeignKey('states.id'), nullable=False),
+    Column('county_id', Integer, ForeignKey('counties.id'), nullable=False),
+)
+
+measurements_table = Table('water_measurements', metadata,
+    Column('id', Integer, primary_key=True),
+    Column('value', Float),
+    Column('body_id', Integer, ForeignKey("water_bodies.id")),
+)
+
 # create tables in database
 metadata.create_all(engine)
